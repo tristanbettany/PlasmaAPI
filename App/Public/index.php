4adef4a674a8fecea158d5b8fdecb89c9b04f04a
Tristan Bettany <tristan.bettany@gmail.com>
2020-07-26 13:55:23 +0100

    VD function

--- App/Public/index.php
+++ App/Public/index.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+if(function_exists('vd') === false) {
+    function vd() {
+        array_map(function($x) {
+            var_dump($x);
+        }, func_get_args());
+        die;
+    }
+}
+
 require_once __DIR__ . '/../../vendor/autoload.php';
 
 use App\Core\Kernel;
